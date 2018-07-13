5a09a458fb0ee2667055d85ac2fc74c2376cd33b
Federico Cardozo <fedecar94@outlook.com>
2018-07-13 17:01:16 -0400

    docs actualizado

---
+++ docs/precache-manifest.f0e957ed401578c0b2e626707def062c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d31363f224d943af37c",
+    "url": "/js/chunk-vendors.7d31363f.js"
+  },
+  {
+    "revision": "2963bef12c9be29d3aac",
+    "url": "/js/app.2963bef1.js"
+  },
+  {
+    "revision": "e4848098f548cff122c8d966bef3026d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2963bef12c9be29d3aac",
+    "url": "/css/app.2e75dc1b.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05707e7a6fc7a9bb0f1746908a1e0ffd.js"
+  "/precache-manifest.f0e957ed401578c0b2e626707def062c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "votacion"});
